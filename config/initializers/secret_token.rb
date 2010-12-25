29bf2d71fa0f2286b3512bc0b35713e5a419ea95
Christopher Giroir <kelsin@valefor.com>
2010-12-24 20:41:53 -0500

    New sc2 rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sc2::Application.config.secret_token = 'dbe6810e158f6f1535b89aea30ecb530dd1f646c128bd34c7990aaeede2eedc2b25db942746f9fa473c2c41531d5d637f55d3431821a13aab60ae3d8f7c2bda8'
