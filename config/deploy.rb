df4eddfccce4014b6945664061b2b9fd4d07e4bc
Christopher Giroir <kelsin@valefor.com>
2011-01-10 12:03:57 -0500

    no more sudo for cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :repository,  "kelsin@git.kelsin.net:~/repos/sc2.git"
 set :branch, "master"
 set :deploy_via, :remote_cache
 
+set :use_sudo, false
+
 role :web, "kelsin.net"
 role :app, "kelsin.net"
 role :db,  "kelsin.net", :primary => true
@@ -15,16 +17,16 @@ namespace :deploy do
   task :start, :roles => :app do
     run "touch #{current_release}/tmp/restart.txt"
   end
- 
+
   task :stop, :roles => :app do
     # Do nothing.
   end
- 
+
   desc "Restart Application"
   task :restart, :roles => :app do
     run "touch #{current_release}/tmp/restart.txt"
   end
- 
+
   desc "Symlink database and config files"
   task :symlink_shared, :roles => :app do
     ['database.yml', 'initializers/secret_token.rb', 'initializers/session_store.rb'].each do |path|
@@ -32,5 +34,5 @@ namespace :deploy do
     end
   end
 end
- 
+
 after 'deploy:update_code', 'deploy:symlink_shared'
