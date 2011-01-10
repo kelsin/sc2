f3bb87845c9eaa6d498dfe8d2a8afe306edba88b
Christopher Giroir <kelsin@valefor.com>
2011-01-10 11:57:28 -0500

    Added new deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,36 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require "bundler/capistrano"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "sc2"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :scm, :git
+set :repository,  "kelsin@git.kelsin.net:~/repos/sc2.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+role :web, "kelsin.net"
+role :app, "kelsin.net"
+role :db,  "kelsin.net", :primary => true
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+ 
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+ 
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+ 
+  desc "Symlink database and config files"
+  task :symlink_shared, :roles => :app do
+    ['database.yml', 'initializers/secret_token.rb', 'initializers/session_store.rb'].each do |path|
+      run "ln -nfs #{shared_path}/config/#{path} #{release_path}/config/#{path}"
+    end
+  end
+end
+ 
+after 'deploy:update_code', 'deploy:symlink_shared'
